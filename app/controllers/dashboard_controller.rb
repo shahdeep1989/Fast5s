94e7bd17b3c3783c4b2c3406665009b3b78dc9c8
Arth T <arththakkar@gmail.com>
2016-02-21 01:16:06 +0530

    Sign in loop issue fixing

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,12 +1,12 @@
 class DashboardController < ApplicationController
   layout 'admin'
 	def index
-		if user_signed_in?
-		  	if current_user.email != "admin@housie.com"
-		  		redirect_to new_user_session_path
-		  	end
-  		else
-  			redirect_to new_user_session_path
-  		end
+		# if user_signed_in?
+		#   	if current_user.email != "admin@housie.com"
+		#   		redirect_to new_user_session_path
+		#   	end
+  # 		else
+  # 			redirect_to new_user_session_path
+  # 		end
 	end
 end
