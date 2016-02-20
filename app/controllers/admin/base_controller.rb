c9664ef4ee8852183a27bb4a8b0489547f543049
Arth T <arththakkar@gmail.com>
2016-02-21 03:18:17 +0530

    Sign in redirection issue fixes

--- app/controllers/admin/base_controller.rb
+++ app/controllers/admin/base_controller.rb
@@ -1,16 +1,16 @@
 class Admin::BaseController < ApplicationController
-	#,:check_for_admin
+	before_action :authenticate_user!, :check_for_admin
 	layout 'admin'
 	
-	# def check_for_admin
-	# 	if user_signed_in?
-	# 		puts "hi test run"
-	# 	  	if current_user.email != "admin@housie.com"
-	# 	  		redirect_to new_user_session_path
-	# 	  	end
- #  		else
- #  			redirect_to new_user_session_path
- #  		end
-	# end
+	def check_for_admin
+		if user_signed_in?
+			puts "hi test run"
+		  	if current_user.email != "admin@housie.com"
+		  		redirect_to new_user_session_path
+		  	end
+  		else
+  			redirect_to new_user_session_path
+  		end
+	end
 
 end
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,12 +2,4 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
-  before_action :authenticate_user!
-	def after_sign_in_path_for(resource)
-  	if resource.email == "admin@housie.com"
-     	redirect_to admin_root_path
-    else
-    	redirect_to root_path
-    end
-	end
 end
