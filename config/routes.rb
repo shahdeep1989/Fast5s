7dee9387e03e064b48a69bd52dfdbfed7b7ffa4e
shefalee <shefaleechaudhary93@gmail.com>
2016-02-21 00:38:33 +0530

    sign up forgot password layout added

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   
     devise_for :users, controllers: {
-      sessions: 'users/sessions'
+      sessions: 'users/sessions',
+      passwords: 'users/passwords',
+      registrations: 'users/registrations'
       
     }
  
