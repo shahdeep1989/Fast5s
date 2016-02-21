31c7775578f540ee6736bcc4df238211fb92bb7e
shanti <shantilal13@gmail.com>
2016-02-21 15:27:48 +0530

    change winning part

--- app/models/winning_part.rb
+++ app/models/winning_part.rb
@@ -1,4 +1,7 @@
 class WinningPart < ActiveRecord::Base
   belongs_to :game
   has_many :winners , :dependent => :destroy
+
+  has_attached_file :image_part, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
+  validates_attachment_content_type :image_part, content_type: /\Aimage\/.*\Z/
 end
