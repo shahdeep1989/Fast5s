df6c96853ecfd6c26944e33eff4ff9565b209735
shahdeep1989 <shahdeep1989@gmail.com>
2016-02-22 01:58:16 +0530

    Solved issue of set element and added new admin in seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,8 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-User.create(email: 'admin@housie.com', password: '12345678', password_confirmation: '12345678')
+User.create(email: 'admin@housie.com', password: '12345678', password_confirmation: '12345678', :user_type => 1)
 u = User.find_by_email('admin@housie.com')
 u.update_attributes(:user_type => 1)
+
+User.create(email: 'shahdeep1989@gmail.com', password: '12345678', password_confirmation: '12345678', :user_type => 1)
