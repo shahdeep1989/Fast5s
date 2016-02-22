91349f5958b2e982d1bf5d7f7ad265b5ccb56cee
shanti <shantilal13@gmail.com>
2016-02-22 09:33:56 +0530

    Admin added

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -2,7 +2,7 @@ class DashboardController < ApplicationController
   layout 'admin'
 	def index
 		if user_signed_in?
-		  	if current_user.email == "admin@housie.com"
+		  	if current_user.email == "admin@housie.com" || current_user.email == "shahdeep1989@gmail.com"
 		  		redirect_to admin_root_path
 		  	end
   		else
